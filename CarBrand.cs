65a339ff0e88e3630144bbafb0d2f11cb6cb9365
RajVetri <135342004+RajVetri@users.noreply.github.com>
2023-06-13 22:53:37 +0530

    Add files via upload

---
+++ CarBrand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carwale
+{
+    internal class CarBrand
+    {
+        String name = "Welcome To CarWale";
+        public void Main()
+        {
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+
+    }
+
+    internal class BMW
+    {
+       
+        String name3 = "BMW M340i";
+        String price3 = "6,850,000"; 
+        String name4 = "BMW 7 Series";
+        String price4 = "17,850,000";
+        public void M40i()
+        {
+            String name = "BMW M40i";
+            String price = "8,650,000";
+            {
+                Console.WriteLine("Model: " + name);
+                Console.WriteLine("Starting from: " + price);
+                Console.ReadLine();
+            }
+        }
+        public void M2()
+        {
+            String name = "BMW M2";
+            String price = "9,850,000";
+            {
+                Console.WriteLine("Model: " + name);
+                Console.WriteLine("Starting from: " + price);
+                Console.ReadLine();
+            }
+        }
+        public void M340i()
+        {
+            Console.WriteLine("Model: " + name3);
+            Console.WriteLine("Starting from: " + price3);
+            Console.ReadLine();
+        }  
+        public void Series7()
+        {
+            Console.WriteLine("Model: " + name4);
+            Console.WriteLine("Starting from: " + price4);
+            Console.ReadLine();
+        }
+
+    }
+}
+    internal class BMW
+    {
+        String name = "";
+        public void Main()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(name);
+        }
+
+}
